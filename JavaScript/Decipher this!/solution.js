9eaae6026aae0b0f029106a6fb7ba1aca00344cc
Natalie Pina <natalie.a.pina@gmail.com>
2020-06-23 10:49:29 -0500

    add decipher this

---
+++ JavaScript/Decipher this!/solution.js
@@ -0,0 +1,10 @@
+function decipherThis(str) {
+  return str
+    .split(" ")
+    .map((word) => {
+      return word
+        .replace(/^\d+/, (char) => String.fromCharCode(char))
+        .replace(/^(.)(.)(.*)(.)$/, "$1$4$3$2");
+    })
+    .join(" ");
+}
